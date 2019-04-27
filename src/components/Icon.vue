eac72de8c3f771b07512db437b26904cf76a5438
Renato Cardoso <re2005@gmail.com>
2019-04-27 11:30:28 +0200

    feat(Icon): add

---
+++ src/components/Icon.vue
@@ -0,0 +1,60 @@
+<template>
+    <!-- eslint-disable vue/no-v-html -->
+    <i
+        class="root"
+        :class="size"
+        v-on="$listeners"
+        v-html="require(`!svg-inline-loader!../assets/icons/icon-${name}.svg`)"
+    />
+</template>
+
+<script>
+    export default {
+        name: 'Icon',
+        props: {
+            name: {
+                type: String,
+                required: true
+            },
+            size: {
+                type: String,
+                required: false,
+                default: 'md'
+            }
+        }
+    };
+</script>
+
+<style lang="scss" scoped>
+    .root {
+        box-sizing: content-box;
+        display: inline-block;
+        font-size: 0;
+    }
+
+    .xs {
+        height: .8rem;
+        width: .8rem;
+    }
+
+    .sm {
+        height: 1rem;
+        width: 1rem;
+    }
+
+    .md {
+        height: 1.2rem;
+        width: 1.2rem;
+    }
+
+    .lg {
+        height: 1.4rem;
+        width: 1.4rem;
+    }
+
+    .xl {
+        height: 1.6rem;
+        width: 1.6rem;
+    }
+
+</style>
