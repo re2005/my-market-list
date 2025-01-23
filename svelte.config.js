cd30b064e1656bf31dd3c7cf409e38c037ac7d94
Renato Cardoso <re2005@gmail.com>
2025-01-23 02:09:58 +0100

    feat: PWA

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { SvelteKitPWA } from '@vite-pwa/sveltekit';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -11,8 +12,16 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
-	}
+		adapter: adapter(),
+		serviceWorker: {
+			register: true
+		}
+	},
+	plugins: [
+		SvelteKitPWA({
+			mode: 'production'
+		})
+	]
 };
 
 export default config;
