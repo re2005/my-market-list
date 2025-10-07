0444fcbeda48386d28d4800a4ad46adfc0c3a751
Renato Cardoso <re2005@gmail.com>
2025-10-07 22:04:23 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,16 @@ import path from 'path';
 import { defineConfig } from 'vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { SvelteKitPWA } from '@vite-pwa/sveltekit';
+import type { ManifestOptions } from 'vite-plugin-pwa';
 import manifest from './manifest.json';
+
+const pwaManifest: Partial<ManifestOptions> = manifest as Partial<ManifestOptions>;
 
 export default defineConfig({
 	plugins: [
 		sveltekit(),
 		SvelteKitPWA({
-			manifest,
+			manifest: pwaManifest,
 			registerType: 'autoUpdate',
 			minify: false,
 			workbox: {
